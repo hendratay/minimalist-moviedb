a68d76f1b28b0cfe511623dfec0d41d490bf03e0
hendratay <hendratay96@gmail.com>
2018-08-13 14:55:05 +0700

    add all bottom navigation fragment

--- app/src/main/java/com/example/hendratay/whatowatch/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/hendratay/whatowatch/presentation/view/activity/MainActivity.kt
@@ -4,13 +4,17 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.util.Log
+import android.view.View
 import com.example.hendratay.whatowatch.R
 import com.example.hendratay.whatowatch.presentation.data.Resource
 import com.example.hendratay.whatowatch.presentation.model.PopularMovieView
+import com.example.hendratay.whatowatch.presentation.view.fragment.*
 import com.example.hendratay.whatowatch.presentation.viewmodel.PopularMovieViewModel
 import com.example.hendratay.whatowatch.presentation.viewmodel.PopularMovieViewModelFactory
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +26,57 @@ class MainActivity : AppCompatActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawBehindStatusBar()
+        setupBottomNavView()
+        getPopularMovie()
+    }
 
+    override fun onBackPressed() {
+        if(bottom_navigation_view.selectedItemId == R.id.action_home) finish()
+        else bottom_navigation_view.selectedItemId = R.id.action_home
+    }
+
+    private fun drawBehindStatusBar() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    }
+
+    private fun setupBottomNavView() {
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.action_explore -> {
+                    loadFragment(ExploreFragment())
+                    true
+                }
+                R.id.action_movie -> {
+                    loadFragment(MovieFragment())
+                    true
+                }
+                R.id.action_tv -> {
+                    loadFragment(TvFragment())
+                    true
+                }
+                R.id.action_user -> {
+                    loadFragment(UserFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment, fragment::class.simpleName)
+                .addToBackStack(fragment::class.simpleName)
+                .commit()
+    }
+
+    private fun getPopularMovie() {
         popularMovieViewModel = ViewModelProviders.of(this, popularMovieViewModelFactory)[PopularMovieViewModel::class.java]
         popularMovieViewModel.getPopularMovie().observe(this,
                 Observer<Resource<PopularMovieView>> {
---
+++ app/src/main/java/com/example/hendratay/whatowatch/presentation/view/fragment/UserFragment.kt
@@ -0,0 +1,16 @@
+package com.example.hendratay.whatowatch.presentation.view.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hendratay.whatowatch.R
+
+class UserFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+}
